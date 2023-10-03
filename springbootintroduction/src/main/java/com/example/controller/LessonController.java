a59ab6c9a63ddc5252edb551a568ad3f2c9ae71f
Kohki Homma <loop.skydriver@hotmail.co.jp>
2023-10-03 19:53:04 +0900

    Lesson02実装完了

--- springbootintroduction/src/main/java/com/example/controller/LessonController.java
+++ springbootintroduction/src/main/java/com/example/controller/LessonController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,9 +10,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/lesson")
 public class LessonController {
 
-    @GetMapping("/sample")
+	@GetMapping("/")
     @ResponseBody
-    public String sample() {
+    public String index() {
         return "Hello World!";
     }
+	
+	@GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Good Evening!";
+    }
+	
+	@GetMapping("/test/{num}")
+	public String test(@PathVariable Integer num) {
+		
+	}
+	
+	@GetMapping("/find/{num}")
+	public String test(@PathVariable(name = "num") Integer number) {
+		
+	}
 }
+
