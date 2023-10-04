054d3a9d181aba49b205a67e7ac839c6f305fdcd
Kohki Homma <loop.skydriver@hotmail.co.jp>
2023-10-04 21:28:18 +0900

    Lesson04実装完了

--- springbootintroduction/src/main/java/com/example/controller/LessonController.java
+++ springbootintroduction/src/main/java/com/example/controller/LessonController.java
@@ -3,17 +3,27 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.form.SampleForm;
 
 @Controller
 @RequestMapping("/lesson")
 public class LessonController {
-
-	@GetMapping("/sample")
-	public String sample(Model model) {
-		String text = "Hello Spring Boot!!";
-		model.addAttribute("message", text);
-        return "index";
-    }
-}
-
+		
+	@PostMapping("/request_test")
+	@ResponseBody
+	public String postTest(@ModelAttribute SampleForm sampleForm) {
+	    return "名前: " + sampleForm.getName() + "<br>血液型: " + sampleForm.getBloodType();
+	    
+	}
+	   
+	    @GetMapping("/form_test")
+	    public String formTest(SampleForm sampleForm, Model model) {
+	        model.addAttribute("sampleForm", sampleForm);
+	        return "lesson/form_test";
+	    }
+	}
