94f6320128b50930f111378852dad0800073f1dc
Kohki Homma <loop.skydriver@hotmail.co.jp>
2023-10-10 20:54:45 +0900

    Lesson07実装完了

--- springbootintroduction/src/main/java/com/example/service/EmployeeService.java
+++ springbootintroduction/src/main/java/com/example/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.example.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.entity.Employee;
 import com.example.repository.EmployeeRepository;
 
 @Service
@@ -12,5 +15,9 @@ public class EmployeeService {
 	@Autowired
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+	}
+        
+        public List<Employee> findAllEmployee() {
+        	return this.employeeRepository.findAll();
     }
 }
